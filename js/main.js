b7aefb7b1953b3ea792546a52ae92dc5c545431f
Jennifer Beatty <jbeatty@southlandind.com>
2014-02-10 14:16:39 -0500

    Building out the Fade and Restore buttons with functionality via jQuery.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-$("button").click(function(){
-    $("img").animate({
-        opacity: 0.5
+$(document).ready(function(){
+
+    $(".fade-img").click(function(){
+        $("img").animate({
+            opacity: 0.5,
+        });
+        $(".fade-img").hide("slow");
+        $(".restore-img").show("slow");
     });
+
+    $(".restore-img").click(function(){
+        $("img").animate({
+            opacity: 1.0,
+        });
+        $(".restore-img").hide("slow");
+        $(".fade-img").show("fast");
+    });
+
 });
